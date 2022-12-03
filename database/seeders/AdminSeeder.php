5cd0f962d4afac80ffe8c48be275f94b363afa27
Eng.Wesam <wesam@engwesams-mbp.mynet>
2022-12-03 02:27:24 +0200

    first commit

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Wesam',
+            'email' => 'admin@nova.com',
+            'password' => bcrypt('password'),
+        ]);
+    }
+}
---
+++ resources/views/layouts/js.blade.php
@@ -0,0 +1,19 @@
+<!--begin::Javascript-->
+<script>var hostUrl = "assets/";</script>
+<!--begin::Global Javascript Bundle(used by all pages)-->
+<script src="{{asset('assets/plugins/global/plugins.bundle.js')}}"></script>
+<script src="{{asset('assets/js/scripts.bundle.js')}}"></script>
+<!--end::Global Javascript Bundle-->
+<!--begin::Page Vendors Javascript(used by this page)-->
+<script src="{{asset('assets/plugins/custom/fullcalendar/fullcalendar.bundle.js')}}"></script>
+<script src="{{asset('assets/plugins/custom/datatables/datatables.bundle.js')}}"></script>
+<!--end::Page Vendors Javascript-->
+<!--begin::Page Custom Javascript(used by this page)-->
+<script src="{{asset('assets/js/widgets.bundle.js')}}"></script>
+<script src="{{asset('assets/js/custom/widgets.js')}}"></script>
+<script src="{{asset('assets/js/custom/apps/chat/chat.js')}}"></script>
+<script src="{{asset('assets/js/custom/utilities/modals/upgrade-plan.js')}}"></script>
+<script src="{{asset('assets/js/custom/utilities/modals/create-app.js')}}"></script>
+<script src="{{asset('assets/js/custom/utilities/modals/users-search.js')}}"></script>
+<!--end::Page Custom Javascript-->
+<!--end::Javascript-->
